ae96e370f70ab975b720fb2b41f9e68a8d0f98ed
V F <vf@Vs-MacBook-Air.local>
2017-05-06 22:11:15 -0700

    2nd Commit

--- EmojiDict/ViewController.swift
+++ EmojiDict/ViewController.swift
@@ -6,20 +6,35 @@
 //  Copyright © 2017 V F. All rights reserved.
 // Hi
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var tableGuy: UITableView!
+    var emojis = ["😍","🖕🏽","👻","🐼","🤓","👹"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableGuy.dataSource = self
+        tableGuy.delegate = self
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
